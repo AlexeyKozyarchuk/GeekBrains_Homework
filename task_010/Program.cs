6803ab7d776f438b4d339c13cff1f559165540d4
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-11 22:48:19 +0500

    Finished task_010

---
+++ task_010/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+           и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1       */
+
+System.Console.WriteLine("Здравствуйте, эта программа покажет" 
+                    +   " Вам вторую цифру введённого Вашего трёхзначного числа,\n"
+                    +   "Введите, пожалуйста, положительное трёхзначное число: ");
+string numberEntered = Console.ReadLine();
+
+void getSecondDigitOfNumber(string numberThreeDigit)
+{
+    System.Console.WriteLine();
+    System.Console.WriteLine($"Вторая цифра ---> {numberThreeDigit[1]}");
+}
+
+getSecondDigitOfNumber(numberEntered);

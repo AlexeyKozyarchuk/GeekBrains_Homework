79c5b6a664bc3379e4e1df8c7fc36034bbbf6919
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-11 23:04:42 +0500

    Finished doing task_013

--- task_013/Program.cs
+++ task_013/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+           или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6         */
+
+System.Console.WriteLine("Здравствуйте, эта программа покажет" 
+                    +   " Вам третью цифру введённого Вашего числа или покажет её отсутствие,\n"
+                    +   "Введите, пожалуйста, положительное число: ");
+string numberEntered = Console.ReadLine();
+
+void getThirdDigitOfNumber(string number)
+{
+    if (number.Length < 3) 
+    {   
+        System.Console.WriteLine();
+        System.Console.WriteLine($"{number} -> третьей цифры нет");
+    }
+    else
+    {
+        System.Console.WriteLine();
+        System.Console.WriteLine($"{number} -> {number[2]}");
+    }
+}
+
+getThirdDigitOfNumber(numberEntered);

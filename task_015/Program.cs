9988324f0aedfc26359107a12080d17a6f60e6cf
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-12 00:25:09 +0500

    Finished doing task_015

--- task_015/Program.cs
+++ task_015/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, 
+           обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет       */
+
+void isWeekend()
+{
+    System.Console.WriteLine("Здравствуйте, эта программа ответит"
+            +   " Вам на вопрос является ли введённое число выходным днём,\n"
+            +   "Введите, пожалуйста, число от 1 до 7 (включительно): ");
+    int dayOfWeek = int.Parse(Console.ReadLine());
+    if ((dayOfWeek == 6 || dayOfWeek == 7))
+    {
+        System.Console.WriteLine($"\n{dayOfWeek} -> да");
+    }
+    if ((dayOfWeek == 1) || (dayOfWeek == 2) || (dayOfWeek == 3) ||(dayOfWeek == 4) || (dayOfWeek == 5))
+    {
+        System.Console.WriteLine($"\n{dayOfWeek} -> нет");
+    }
+    else
+    {
+        System.Console.WriteLine($"\n{dayOfWeek} -> Вы ввели недопустимое значение");
+    }
+}
+
+isWeekend();

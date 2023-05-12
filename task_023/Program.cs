8b768b7a6bc9829b2ce06771dabcb518a712b95b
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-12 10:31:56 +0500

    .\task_023\

--- task_023/Program.cs
+++ task_023/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 23: Напишите программу, которая принимает на вход число (N)
+              и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125          */
+
+void getNumbersCup()
+{
+    System.Console.WriteLine("Здравствуйте, эта программа покажет"
+            +   " Вам числа в кубе от 1 до введённого Вами числа,"
+            +   "\nВведите, пожалуйста, число: ");
+    int number = int.Parse(Console.ReadLine());
+    System.Console.WriteLine();
+    for (int i = 1; i <= number; i++)
+    {
+        double result = Math.Pow(i, 3);
+        if (i == number)
+        {
+            System.Console.Write(result);
+            break;
+        }
+        System.Console.Write($"{result}, ");
+    }
+}
+
+getNumbersCup();

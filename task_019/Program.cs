09bbae5336aa5635f276fff230a0b129493a93c3
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-12 09:02:10 +0500

    Finished doing task_019

--- task_019/Program.cs
+++ task_019/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 19: Напишите программу, которая принимает на вход пятизначное число 
+              и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+12821 -> да
+23432 -> да      */
+
+void isPalindrome()
+{
+    System.Console.WriteLine("Здравствуйте, эта программа ответит"
+            +   " Вам на вопрос является ли введённое пятизначное число палиндромом,\n"
+            +   "Введите, пожалуйста, пятизначное число: ");
+    string numberEntered = Console.ReadLine();
+    int length = numberEntered.Length;
+    int index = -1;
+        if (numberEntered[index + 1] == numberEntered[length - 1]);
+        if (numberEntered[index + 1] == numberEntered[length - 1]) System.Console.WriteLine($"\n{numberEntered} -> да");
+        else System.Console.WriteLine($"\n{numberEntered} -> нет");
+}
+
+isPalindrome();

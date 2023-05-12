33b00b27c0b00d2a46a0e975bec63677f0ba692e
Alexey <a.a.kozyarchuk@gmail.com>
2023-05-12 18:15:25 +0500

    Finished doing task_025

--- task_025/Program.cs
+++ task_025/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 25:       Напишите цикл, который принимает на вход два числа (A и B)
+                    и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+2, 4 -> 16   */
+
+void raiseOneNumberToAnother()
+{
+    System.Console.WriteLine("Здравствуйте, эта программа"
+            +   " для Вас возведёт первое число в степень второго числа,"
+            +   "\nВведите, пожалуйста, первое число(основание степени): ");
+    int numberFirst = int.Parse(Console.ReadLine());
+
+        System.Console.WriteLine("\nВведите, пожалуйста, второе число(показатель степени): ");
+    int numberSecond = int.Parse(Console.ReadLine());
+
+    double result = Math.Pow(numberFirst, numberSecond);
+    System.Console.WriteLine($"{numberFirst}, {numberSecond} -> {result}");
+}
+
+raiseOneNumberToAnother();
